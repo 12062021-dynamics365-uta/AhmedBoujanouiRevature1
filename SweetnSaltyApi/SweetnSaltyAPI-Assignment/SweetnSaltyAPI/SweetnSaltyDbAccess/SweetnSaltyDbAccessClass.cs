82fc89619134aaf2e3896195f439ce343d74d767
jimie2021 <ahmed.boujanoui@revature.net>
2022-01-11 00:24:46 -0500

    UNFINISHED COMMIT

---
+++ SweetnSaltyApi/SweetnSaltyAPI-Assignment/SweetnSaltyAPI/SweetnSaltyDbAccess/SweetnSaltyDbAccessClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace SweetnSaltyDbAccess
+{
+    public class SweetnSaltyDbAccessClass : ISweetnSaltyDbAccessClass
+    {
+        private readonly string str = ""; /*connection string here.*/
+        private readonly SqlConnection _con;
+
+        //constructor
+        public SweetnSaltyDbAccessClass()
+        {
+            this._con = new SqlConnection(this.str);
+            _con.Open();
+        }
+
+        public async Task<SqlDataReader> PostFlavor(string flavorname)
+        {
+            string sqlQuery = $"INSERT INTO Flavor VALUES (@flavorname) ";
+
+            using (SqlCommand cmd = new SqlCommand(sqlQuery, this._con))
+            {
+                cmd.Parameters.AddWithValue("@flavorname", flavorname);
+
+                try
+                {
+                    await cmd.ExecuteNonQueryAsync();
+                    string retrieveFlavor = "SELECT TOP 1 * FROM Flavor ORDER BY FlavorId DESC;";
+                    using (SqlCommand cmd1 = new SqlCommand(retrieveFlavor, this._con))
+                    {
+
+                        SqlDataReader dr = await cmd1.ExecuteReaderAsync();
+                        return dr;
+                    }
+                }
+                catch (DbException ex)// TODO do something with this exception
+                {
+                    Console.WriteLine($"There was an exception in SweetnSaltyBusinessClass.PostFlavor{ex}");
+                    return null;
+                }
+            }
+        }
+
+
+    }
+}
